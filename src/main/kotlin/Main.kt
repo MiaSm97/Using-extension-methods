ddd5fbd9b4594c432f54f2e6d10ce2965558e4be
Mia Smaldone <eufosm@libero.it>
2023-01-18 10:24:58 +0100

    rimossa data class

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,18 +3,14 @@ Write an extension method that prints list values.
 Then create a list that that has at least 10 members, filter them and then print the values that pass the filter.*/
 
 fun main(){
-    data class Values(val param: List<Int>)
 
-    fun Values.filterSmaller() : List<Int> {
-        return param.filter { it < 500 }
+    fun List<Int>.filterSmaller() : List<Int> {
+        return this.filter { it < 500 }
     }
-    fun Values.listValues() : List<Int>{
-        return param
+    fun List<Int>.printSmaller() {
+        this.forEach { println(it) }
     }
 
-    val test = Values(listOf(1, 4578, 75, 420, 78203, 5, 22, 56394, 0, 123))
-    println(test.listValues())
-    println(test.filterSmaller())
-
-
+    val test = listOf(1, 4578, 75, 420, 78203, 5, 22, 56394, 0, 123)
+    test.filterSmaller().printSmaller()
 }
